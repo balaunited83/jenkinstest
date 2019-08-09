a16e12411cfb543e791b0e2ace4fef941e351ed8
Bala Rao <balasubrahmanya.rao@united.com>
2019-08-08 23:09:17 -0500

    new files for jenkins test

---
+++ src/helloworld/HelloworldJenkinsTest.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class HelloworldJenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<10;i++) {
+			
+			System.out.println("Hello world Jenkins Git Test "+i);
+			
+		}
+
+	}
+
+}
